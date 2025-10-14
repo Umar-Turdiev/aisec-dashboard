6c177d0005bd03e28289c2da5a5ad08caaab9212
Umar Turdiev <ukcturdiev@gmail.com>
2025-10-14 09:37:43 -0700

    Pushed environment file with redacted fields

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,27 @@
 export const environment = {
-  production: true,
-  api: {
-    startScanUrl:
-      'https://dchqaf7nl4fdpftoluotjr2kqe0qitnd.lambda-url.us-east-1.on.aws/',
-    logsUrl: 'https://4g74bplbcend4w377rysdpscty0mptif.lambda-url.us-east-1.on.aws/',
-    resultUrl: '',
+  production: false,
+  aws: {
+    region: 'us-east-2',
+  },
+  bedrock: {
+    // bedrock IAM user access key
+    accessKeyId: '',
+    secretAccessKey: '',
+    modelId: 'anthropic.claude-3-5-sonnet-20241022-v2:0',
+    inferenceProfileArn: '',
+  },
+  lambdaEndpoints: {
+    // need to add lambda endpoint to make it work
+    startSemgrepScanUrl:
+      '',
+    semgrepScannerLogsUrl:
+      '',
+    startHarnessPipelineURL:
+      '',
+    harnessLogsURL:
+      '',
+    fetchResultUrl:
+      '',
   },
   demo: { mockMode: false },
 };
